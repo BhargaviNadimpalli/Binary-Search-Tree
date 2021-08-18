21b2019656809e7cf4d14f889a97e04436e24025
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-17 21:18:12 -0600

    feat: created binary tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,8 +9,19 @@ namespace BinarySearchTree
             Console.WriteLine("Welcome to Binary search tree programs");
 
             BinaryTree<int> search = new BinaryTree<int>(56);
+          
             search.Insert(30);
             search.Insert(70);
+            search.Insert(22);
+            search.Insert(40);
+            search.Insert(60);
+            search.Insert(95);
+            search.Insert(11);
+            search.Insert(65);
+            search.Insert(3);
+            search.Insert(16);
+            search.Insert(63);
+            search.Insert(67);
             search.Display();
             search.GetSize();
         }
