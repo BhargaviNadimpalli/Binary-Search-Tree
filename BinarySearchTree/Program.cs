25d51fd5e23f7c6c1f2399a3e37bd3135d0df188
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-17 21:09:44 -0600

    feat: Created BST by adding values

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,12 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary search tree programs");
+
+            BinaryTree<int> search = new BinaryTree<int>(56);
+            search.Insert(30);
+            search.Insert(70);
+            search.Display();
+            search.GetSize();
         }
     }
 }
